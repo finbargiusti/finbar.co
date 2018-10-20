c040c3039a61c7108fae28aa8b08909b2f77a111
finbargiusti <finbargiusti@gmail.com>
2018-10-20 21:30:16 +0100

    Added main content and mobile support

--- js/index.js
+++ js/index.js
@@ -8,14 +8,24 @@ $.each(letters, (index, element) => {
     $( element ).click(() => {
         if (!open) {
             open = true;
-            $(element).css("left", "0")
             $($(element).children()[0]).css({"background-color": "hsl("+(60*(index+1))+", 40%, 50%)", "color": "white" })
-            for (let i = 0; i < letters.length - index -2; i++) {
-                $(letters[index+1+i]).css("left", "100vw")
+            if ($(window).width() > 1100) {
+                $(element).css("left", "0")
+                for (let i = 0; i < letters.length - index -2; i++) {
+                    $(letters[index+1+i]).css("left", "100vw")
+                }
+                $(".close").css("z-index", index+1)
+            } else {
+                $(element).css("bottom", "0")
+                for (let i = index-1; i >= 0; i--) {
+                    $(letters[i]).css("bottom", "100vh")
+                }
+                $(".close").css("z-index", Math.abs(6-index))
             }
-            $(".close").css("z-index", index+1)
             $(".container").addClass("fullView")
             $(".container").removeAttr("style")
+            height = window.innerHeight
+            $(".container").css("--height", height)
         }
     })    
 })
@@ -26,7 +36,9 @@ $.each($(".close"), (index, element) => {
             $(elem).removeAttr("style")
             $($(elem).children()[0]).removeAttr("style")
         })
-        $(".container").css("transform", "perspective(600px) rotateY("+rotY+"deg) rotateX("+rotX+"deg) scale(0.6)")
+        if ($(window).width() > 1100) {
+            $(".container").css("transform", "perspective(600px) rotateY("+rotY+"deg) rotateX("+rotX+"deg) scale(0.6)")
+        }
         setTimeout(() => {
             open = false
         }, 200)
@@ -36,21 +48,43 @@ $.each($(".close"), (index, element) => {
     })
 })
 
-let centerY,centerX, mouseX, mouseY, rotY, rotX
+let centerY,centerX, mouseX, mouseY, rotY, rotX, height
 
 $(document).ready(() => {
+    height = window.innerHeight
+    $(".container").css("--height", height)
+    window.screen.orientation.lock("portrait")
     $(document).mousemove((e) => {
-        centerY = $(window).height() / 2
-        centerX = $(window).width() / 2
+        if ($(window).width() > 1100) {
+            centerY = $(window).height() / 2
+            centerX = $(window).width() / 2
 
-        mouseX = e.pageX - centerX
-        mouseY = (e.pageY - centerY) * -1
+            mouseX = e.pageX - centerX
+            mouseY = (e.pageY - centerY) * -1
 
 
-        rotY = (mouseX / centerX) * 8 // 5
-        rotX = (mouseY / centerY) * 13 // 8
-        if (!open) {
-            $(".container").css("transform", "perspective(600px) rotateY("+rotY+"deg) rotateX("+rotX+"deg) scale(0.6)")
+            rotY = (mouseX / centerX) * 8 // 5
+            rotX = (mouseY / centerY) * 13 // 8
+            if (!open) {
+                $(".container").css("transform", "perspective(600px) rotateY("+rotY+"deg) rotateX("+rotX+"deg) scale(0.6)")
+            }
         }
     })
 })
+
+$(window).resize(() => {
+    height = window.innerHeight
+    $(".container").css("--height", height)
+})
+
+setInterval(() => {
+    var options = {
+        timeZone: 'Europe/Dublin',
+        hour: 'numeric', minute: 'numeric', second: 'numeric',
+    }
+    let formatter = new Intl.DateTimeFormat([], options)
+    let currTime = formatter.format(new Date())
+    $("#time").html(
+        currTime
+    )
+}, 1000)
