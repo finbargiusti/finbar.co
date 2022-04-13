d81aa99352923c64aabfa54f80e90f053ce86b5d
finbargiusti <finbargiusti@gmail.com>
2022-04-13 23:36:04 +0100

    new changes

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import ViteFonts from 'vite-plugin-fonts';
+import { splitVendorChunkPlugin } from 'vite';
+
+export default {
+  plugins: [
+    ViteFonts({
+      google: {
+        families: [
+          {
+            name: 'Roboto',
+            styles:
+              'ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900',
+          },
+        ],
+      },
+    }),
+  ],
+  build: {
+    target: 'es2015',
+    outDir: 'docs/',
+    rollupOptions: {
+      output: 'compact',
+    },
+  },
+};
